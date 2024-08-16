749ab76b39944ff71cf289a239ea6ccba4c2e8e8
Israel <bamideleisrael04@gmail.com>
2024-08-16 21:01:58 +0100

    main function

--- telecom.c
+++ telecom.c
@@ -116,3 +116,51 @@ void displayMenu()
     printf("6. Exit\n");    
 
 }
+
+
+
+int main()
+{
+    int choice;
+    char phoneNumber[15];
+
+    while (1) {
+        displayMenu();
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                addRecord();
+                break;
+            case 2:
+                viewRecords();
+                break;
+            case 3:
+                printf(
+                    "\nEnter phone number to modify record: ");
+                scanf("%s", phoneNumber);
+                modifyRecord(phoneNumber);
+                break;
+            case 4:
+                printf(
+                    "\nEnter phone number to view payment");
+                scanf("%s", phoneNumber);
+                viewPayment(phoneNumber);
+                break;
+            case 5: 
+                printf(
+                    "\nEnter phone number to delete record: ");
+                scanf("%s", phoneNumber);
+                deleteRecord(phoneNumber);
+                break;
+            case 6:
+                return 0;
+            default:
+                printf("\nInvalid choice! Please try again.\n");
+
+        }
+    }
+
+    return 0;
+}
