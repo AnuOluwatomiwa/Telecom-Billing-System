dad5e227b1702b8155f8fe65cca9618ea0c724a2
Israel <bamideleisrael04@gmail.com>
2024-08-16 02:43:27 +0100

    addRecord(): Function to Add New Customer Record

---
+++ telecom.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+// Structure to hold customer information
+struct Customer {
+    char name[50];
+    char phoneNumber[15];
+    float usage;
+    float totalBill;
+};
+
+// Global array to store customer data
+struct Customer customers[100];
+
+// Global variable to keep track of the number of customers
+int customerCount = 0;
+
+//Function to add a new customer record
+void addRecord()
+{
+    if (customerCount < 100) {
+        printf("\nEnter name: ");
+        scanf("%[^\n]", customers[customerCount].name);
+        printf("\nEnter phone number: ");
+        scanf("%s", customers(customerCount).phoneNumber);
+        printf("Enter usage (in minutes): ");
+        scanf("%f", customers(customerCount).usage);
+        customers(customerCount).totalBill
+            = customers[customerCount].usage * 0.1;
+        customerCount++;
+        printf("\nRecord added successfully!\n");
+    }
+    else {
+        printf("\nMaximum number of records reached!\n");
+    }
+}
