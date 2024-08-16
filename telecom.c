978faeff4fc182be2ba07e75e8f1af0afe0f8be4
Israel <bamideleisrael04@gmail.com>
2024-08-16 20:29:50 +0100

    deleteRecord(): Deleting a Customer Record by Phone Number This function deleteRecord() is responsible for deleting a customer record based on their phone number. It takes a char array phoneNumber as a parameter which is used to identify the customer record to be deleted. If a matching phone number is found, it shifts all records after the matched record by one position to effectively delete the record. After deleting the record it decrements the customerCount by 1. It provides feedback to the user confirming a successful record deletion or informing them the record was not found.

--- telecom.c
+++ telecom.c
@@ -90,3 +90,17 @@ void searchRecord(char phoneNumber[])
     }
     printf("\nRecord not found!\n");
 )
+
+void deleteRecord(char phoneNumber[])
+(
+    for (int i = 0; i < customerCount; i++) {
+        if (strcmp(customers[i].phoneNumber, phoneNumber) == 0)
+            for (int j = i; i < customerCount - 1; j++) {
+                customers[j] = customers[j + 1];
+            }
+            customerCount--;
+            printf("\nRecord deleted successfully!\n");
+            return;
+    }
+    printf("\nRecord not found!\n");
+)
